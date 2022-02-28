34ca4ac48f2531b80d0b647bee6ad03a97f0dbed
lexoxo <axeltraore@gmail.com>
2022-02-28 18:29:08 +0000

    Update menu position and style

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function toogleNavBar() {
     if (nav.style.display !== 'none') nav.style.display = 'none';
     else nav.style.display = 'flex'
 }
+
+let links = document.querySelectorAll('.hamburger-menu a');
+for( let i =0; i< links.length; i++) links[i].addEventListener('click',toogleNavBar);
+document.querySelector('.close').addEventListener('click',toogleNavBar);
+document.querySelector('.mob-menu img').addEventListener('click',toogleNavBar);
