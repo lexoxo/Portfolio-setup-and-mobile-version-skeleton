82999af5f01989ac9ebab9631bfdb3f8b5257495
lexoxo <axeltraore@gmail.com>
2022-03-02 08:23:28 +0000

    Add links for demo and source

--- source/script.js
+++ source/script.js
@@ -1,9 +1,9 @@
-import { works } from "./data.js";
+import { works } from './data.js';
 
 function toogleNavBar() {
-    const nav = document.getElementById('nav');
-    if (nav.style.display !== 'none') nav.style.display = 'none';
-    else nav.style.display = 'flex';
+  const nav = document.getElementById('nav');
+  if (nav.style.display !== 'none') nav.style.display = 'none';
+  else nav.style.display = 'flex';
 }
 
 const links = document.querySelectorAll('.hamburger-menu a');
@@ -11,15 +11,13 @@ for (let i = 0; i < links.length; i += 1) links[i].addEventListener('click', too
 document.querySelector('.close').addEventListener('click', toogleNavBar);
 document.querySelector('.mob-menu img').addEventListener('click', toogleNavBar);
 
+for (let i = 0; i < works.length; i += 1) {
+  let liText = '';
+  for (let j = 0; j < works[i].technolgies.length; j += 1) liText += `<li>${works[i].technolgies[j]}</li>`;
 
-
-for (let i = 0; i < works.length; i++) {
-    let liText = "";
-    for (let j = 0; j < works[i].technolgies.length; j++) liText += `<li>${works[i].technolgies[j]}</li>`;
-
-    let work = document.createElement('div');
-    work.classList.add('work');
-    work.innerHTML = `
+  const work = document.createElement('div');
+  work.classList.add('work');
+  work.innerHTML = `
   <img src="${works[i].imageSrc}" alt="project overwiew image" />
             <div class="work-text">
               <h2 class="work-description">${works[i].titre}</h2>
@@ -29,29 +27,27 @@ for (let i = 0; i < works.length; i++) {
               <div class="work-button" id = "${works[i].id}">See Project</div>
             </div>
   `;
-    document.querySelector('.works').appendChild(work);
+  document.querySelector('.works').appendChild(work);
 }
 
-
-let button = document.querySelectorAll('.work-button');
-let previousBody = document.body;
+const previousBody = document.body;
 
 function pop(event) {
-    let clickButton = event.target.getAttribute('id');
-    let currentWork = [];
-    works.forEach(function(o) {
-        if (clickButton == o.id) currentWork = o;
-    });
-    let popup = document.createElement('div');
-    let liTech = "";
-    for (let j = 0; j < currentWork.popupDescription.technolgies.length; j++) liTech += `<li class = 'popup-li'>${currentWork.popupDescription.technolgies[j]}</li>`;
+  const clickButton = event.target.getAttribute('id');
+  let currentWork = [];
+  works.forEach((o) => {
+    if (clickButton === o.id) currentWork = o;
+  });
+  const popup = document.createElement('div');
+  let liTech = '';
+  for (let j = 0; j < currentWork.popupDescription.technolgies.length; j += 1) liTech += `<li class = 'popup-li'>${currentWork.popupDescription.technolgies[j]}</li>`;
 
-    popup.classList.add('popup');
-    popup.setAttribute('id', 'popup');
-    popup.innerHTML = `
+  popup.classList.add('popup');
+  popup.setAttribute('id', 'popup');
+  popup.innerHTML = `
 <div class="popup-container">
 <div class="project-img">
-    <img src="images/popup-cancel.png" class="closeTab">
+    <i class="fa-solid fa-xmark closeTab"></i>
   <img src="${currentWork.popupDescription.imageSrc}" class="popup-image">
 </div>
 
@@ -60,13 +56,13 @@ function pop(event) {
   ${currentWork.popupDescription.titre}
   </h2>
   <div class="popup-buttons-desk">
-    <button type="button" class="see-live">
+    <button id="see-live" type="button" class="see-live">
       <h6>See Live</h6>
-      <a href="#"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+      <i class="fa-solid fa-arrow-up-right-from-square"></i>
     </button>
-    <button type="button" class="see-live">
+    <button id="see-source" type="button" class="see-live">
       <h6>See Source</h6>
-      <a href="#"><i class="fa fa-github"></i></a></button>
+      <i class="fa fa-github"></i></button>
   </div>
 </div>
 
@@ -81,27 +77,35 @@ function pop(event) {
  ${currentWork.popupDescription.description}
 </p>
 <div class="popup-buttons">
-  <button type="button" class="see-live">
+  <button id="see-live" type="button" class="see-live">
     <h6>See Live</h6>
     <i class="fa fa-github"></i>
   </button>
-  <button type="button" class="see-live">
+  <button id="see-source" type="button" class="see-live">
     <h6>See Source</h6>
     <i class="fa fa-github"></i></button>
 </div>
 </div>
-
 `;
-    let newbody = document.createElement('body');
-    newbody.appendChild(popup);
-    document.body = newbody;
-    document.querySelector('.closeTab').addEventListener('click', function() {
-        document.body = previousBody;
-        window.location.href = 'index.html#recentWork';
-    })
+  const newbody = document.createElement('body');
+  newbody.appendChild(popup);
+  document.body = newbody;
+
+  document.querySelector('.closeTab').addEventListener('click', () => {
+    document.body = previousBody;
+    window.location.href = 'index.html#recentWork';
+  });
+
+  document.querySelector('#see-live').addEventListener('click', () => {
+    window.open(currentWork.popupDescription.demoLink);
+  });
+
+  document.querySelector('#see-source').addEventListener('click', () => {
+    window.open(currentWork.popupDescription.source);
+  });
 }
 
-let clickClick = document.querySelectorAll('.work-button');
-clickClick.forEach(function(e) {
-    e.addEventListener('click', pop);
-})
+const clickClick = document.querySelectorAll('.work-button');
+clickClick.forEach((e) => {
+  e.addEventListener('click', pop);
+});
