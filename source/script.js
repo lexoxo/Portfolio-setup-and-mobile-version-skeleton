432198247770feb0b7bfce2ab279f68000be0d2c
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-03 12:42:50 +0100

    Debbugg JavaScript

--- source/script.js
+++ source/script.js
@@ -1,9 +1,9 @@
 import { works } from './data.js';
 
 function toogleNavBar() {
-  const nav = document.querySelector('.hamburger');
-  if (nav.style.display !== 'none') nav.style.display = 'none';
-  else nav.style.display = 'flex';
+    const nav = document.querySelector('.hamburger');
+    if (nav.style.display !== 'none') nav.style.display = 'none';
+    else nav.style.display = 'flex';
 }
 
 const links = document.querySelectorAll('.hamburger-menu a');
@@ -12,12 +12,12 @@ document.querySelector('.close').addEventListener('click', toogleNavBar);
 document.querySelector('.mob-menu img').addEventListener('click', toogleNavBar);
 
 for (let i = 0; i < works.length; i += 1) {
-  let liText = '';
-  for (let j = 0; j < works[i].technolgies.length; j += 1) liText += `<li>${works[i].technolgies[j]}</li>`;
+    let liText = '';
+    for (let j = 0; j < works[i].technolgies.length; j += 1) liText += `<li>${works[i].technolgies[j]}</li>`;
 
-  const work = document.createElement('div');
-  work.classList.add('work');
-  work.innerHTML = `
+    const work = document.createElement('div');
+    work.classList.add('work');
+    work.innerHTML = `
   <img src="${works[i].imageSrc}" alt="project overwiew image" />
             <div class="work-text">
               <h2 class="work-description">${works[i].titre}</h2>
@@ -27,24 +27,24 @@ for (let i = 0; i < works.length; i += 1) {
               <div class="work-button" id = "${works[i].id}">See Project</div>
             </div>
   `;
-  document.querySelector('.works').appendChilds(work);
+    document.querySelector('.works').appendChild(work);
 }
 
 const previousBody = document.body;
 
 function pop(event) {
-  const clickButton = event.target.getAttribute('id');
-  let currentWork = [];
-  works.forEach((o) => {
-    if (clickButton === o.id) currentWork = o;
-  });
-  const popup = document.createElement('div');
-  let liTech = '';
-  for (let j = 0; j < currentWork.popupDescriptions.technolgies.length; j += 1) liTech += `<li class = 'popup-li'>${currentWork.popupDescription.technolgies[j]}</li>`;
+    const clickButton = event.target.getAttribute('id');
+    let currentWork = [];
+    works.forEach((o) => {
+        if (clickButton === o.id) currentWork = o;
+    });
+    const popup = document.createElement('div');
+    let liTech = '';
+    for (let j = 0; j < currentWork.popupDescription.technolgies.length; j += 1) liTech += `<li class = 'popup-li'>${currentWork.popupDescription.technolgies[j]}</li>`;
 
-  popup.classList.add('popup');
-  popup.setAttribute('id', 'popup');
-  popup.innerHTML = `
+    popup.classList.add('popup');
+    popup.setAttribute('id', 'popup');
+    popup.innerHTML = `
 <div class="popup-container">
 <div class="project-img">
     <i class="fa-solid fa-xmark closeTab"></i>
@@ -87,31 +87,31 @@ function pop(event) {
 </div>
 </div>
 `;
-  const newbody = document.createElement('body');
-  newbody.appendChild(popup);
-  document.body = newbody;
+    const newbody = document.createElement('body');
+    newbody.appendChild(popup);
+    document.body = newbody;
 
-  document.querySelector('.closeTab').addEventListener('click', () => {
-    document.body = previousBody;
-    window.location.href = 'index.html#recentWork';
-  });
-
-  document.querySelectorAll('.see-lives').forEach((e) => {
-    e.addEventListener('click', () => {
-      window.open(currentWork.popupDescription.demoLink);
+    document.querySelector('.closeTab').addEventListener('click', () => {
+        document.body = previousBody;
+        window.location.href = 'index.html#recentWork';
     });
-  });
 
-  document.querySelectorAll('.see-source').forEach((e) => {
-    e.addEventListener('click', () => {
-      window.open(currentWork.popupDescription.source);
+    document.querySelectorAll('.see-lives').forEach((e) => {
+        e.addEventListener('click', () => {
+            window.open(currentWork.popupDescription.demoLink);
+        });
     });
-  });
+
+    document.querySelectorAll('.see-source').forEach((e) => {
+        e.addEventListener('click', () => {
+            window.open(currentWork.popupDescription.source);
+        });
+    });
 }
 
 const clickClick = document.querySelectorAll('.work-button');
 clickClick.forEach((e) => {
-  e.addEventListener('click', pop);
+    e.addEventListener('click', pop);
 });
 
 const form = document.getElementsByTagName('form')[0];
@@ -122,21 +122,21 @@ const submitError = document.querySelector('#submitError');
 const reg = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 email.addEventListener('input', () => {
-  console.log('press');
+    console.log('press');
 
-  if (reg.test(email.value)) {
-    emailError.innerHTML = '';
-    email.classList.remove('invalid');
-  } else {
-    email.classList.add('invalid');
-    emailError.innerHTML = 'Email should be in lowercase';
-  }
+    if (reg.test(email.value)) {
+        emailError.innerHTML = '';
+        email.classList.remove('invalid');
+    } else {
+        email.classList.add('invalid');
+        emailError.innerHTML = 'Email should be in lowercase';
+    }
 });
 
 form.addEventListener('submit', (event) => {
-  if (!reg.test(email.value.trim())) {
-    email.classList.add('invalid');
-    submitError.innerHTML = 'The form is not sent. Email should be in lowercase';
-    event.preventDefault();
-  }
+    if (!reg.test(email.value.trim())) {
+        email.classList.add('invalid');
+        submitError.innerHTML = 'The form is not sent. Email should be in lowercase';
+        event.preventDefault();
+    }
 });
